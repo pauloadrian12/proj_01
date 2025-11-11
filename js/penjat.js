ec3e1489c6f6297b3256c20dc217aa5ab6e309b1
Adrian <paulooliva.alum@insestatut.cat>
2025-11-11 19:14:25 +0100

    FaltaMuchoDiseño

--- js/penjat.js
+++ js/penjat.js
@@ -1,14 +1,21 @@
+//poner aqui conexion con paraules.js y sacar una pálabra random
+let palabras = ["uno","dos","tres"]
 window.onload = () => {
     
-    let input = document.getElementById("");
+    let input = document.getElementById("cuadro");
+     for (let i of palabras[2]){
+        input.innerHTML+=  '<div> <input type="text" maxlength="1" style="width: 30px; text-align: center;">    </input>   </div>   '
+     }
    
     let win = window.open("./penjat.html", "", "popup,width=500,height=500");
 
 
-    document.getElementById("button").addEventListener("", () => {
+    document.getElementById("c").addEventListener("", () => {
 
         win.postMessage(input.value, "http://127.0.0.1:5500");
 
     })
 
 }
+
+
