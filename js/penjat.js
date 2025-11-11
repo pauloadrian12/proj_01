25dbf0cd9e6cbd0fa24f7c0dd4f08b580b18760e
Adrian <paulooliva.alum@insestatut.cat>
2025-11-11 18:51:05 +0100

    jugar-penjat

---
+++ js/penjat.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    
+    let input = document.getElementById("");
+   
+    let win = window.open("./penjat.html", "", "popup,width=500,height=500");
+
+
+    document.getElementById("button").addEventListener("", () => {
+
+        win.postMessage(input.value, "http://127.0.0.1:5500");
+
+    })
+
+}
