923e63fbc56c765a1c5c1372cd8f846eb6272a1c
Adrian <paulooliva.alum@insestatut.cat>
2025-11-25 19:13:08 +0100

    temporizador

--- js/penjat.js
+++ js/penjat.js
@@ -13,12 +13,18 @@ let lletresProvades = [];
 let win = window.open("./penjat.html", "", "popup,width=500,height=500");
 
 window.onload = () => {
+    let Temporizador = new Date(0)
 
-  
+
     inicialitzaJoc();
     eval_form()
 
-   
+
+    setInterval(() => {
+        Temporizador.setSeconds(Temporizador.getSeconds()+1)
+        document.getElementById("temp").innerText =  Temporizador.getMinutes() +" : " + Temporizador.getSeconds() ;
+    }, 1000);
+
 }
 
 
