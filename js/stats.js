20da5ec6a688b3473fdebaf0c65defb6c3a88e99
Adrian <paulooliva.alum@insestatut.cat>
2025-12-09 19:01:12 +0100

    cookies

--- js/stats.js
+++ js/stats.js
@@ -1,38 +1,45 @@
-window.onload = function() {
+window.onload = function () {
 
     let statsElement = document.getElementById("stats");
     let stats = JSON.parse(localStorage.getItem("stats"));
-    let stats_cookies = JSON.parse("["+document.cookie.split("=").slice(1).toString()+"]");
+    let stats_cookies = JSON.parse("[" + document.cookie.split("=").slice(1).toString() + "]");
 
-    stats= stats.concat(stats_cookies);
+
+    if (stats) {
+         (stats = stats.concat(stats_cookies)); 
+        }else{
+            stats=stats_cookies
+        }
+
+
     if (stats && stats.length > 0) {
         let files = "";
         for (let i = stats.length - 1; i >= 0; i--) {
             let partida = stats[i];
             files += "<tr>" +
-                        "<td>" + partida.resultat + "</td>" +
-                        "<td>" + partida.vidas + "</td>" +
-                        "<td>" + partida.tiempo + "</td>" +
-                        "<td>" + partida.data + "</td>" +
-                    "</tr>";
+                "<td>" + partida.resultat + "</td>" +
+                "<td>" + partida.vidas + "</td>" +
+                "<td>" + partida.tiempo + "</td>" +
+                "<td>" + partida.data + "</td>" +
+                "</tr>";
         }
 
         let tabla = "<table class='table table-striped-columns'>" +
-                        "<thead>" +
-                            "<tr>" +
-                                "<th scope='col'>Resultado</th>" +
-                                "<th scope='col'>Vidas restantes</th>" +
-                                "<th scope='col'>Tiempo</th>" +
-                                "<th scope='col'>Fecha</th>" +
-                            "</tr>" +
-                        "</thead>" +
-                        "<tbody>" +
-                            files +
-                        "</tbody>" +
-                    "</table>";
+            "<thead>" +
+            "<tr>" +
+            "<th scope='col'>Resultado</th>" +
+            "<th scope='col'>Vidas restantes</th>" +
+            "<th scope='col'>Tiempo</th>" +
+            "<th scope='col'>Fecha</th>" +
+            "</tr>" +
+            "</thead>" +
+            "<tbody>" +
+            files +
+            "</tbody>" +
+            "</table>";
 
         statsElement.innerHTML = tabla;
     } else {
-statsElement.innerHTML = "<div style='border:1px solid #000000ff; padding:5px; width:200px;'>No hay estadísticas disponibles.</div>";
+        statsElement.innerHTML = "<div style='border:1px solid #000000ff; padding:5px; width:200px;'>No hay estadísticas disponibles.</div>";
     }
 };
