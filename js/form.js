e48b2b26867070bfdd2405770b7e44593766ae28
Adrian <paulooliva.alum@insestatut.cat>
2025-12-12 17:09:19 +0100

    a

--- js/form.js
+++ js/form.js
@@ -46,11 +46,7 @@ function analizar(dat, pal, vidas) {
 
 
 
-/**
- * 
- * 
- * 
- */
+
 function eval_form() {
     let form = document.getElementById("lletra")
     let bloque_mensaje = document.getElementById("mensaje")
